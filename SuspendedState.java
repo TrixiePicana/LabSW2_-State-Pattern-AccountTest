e9623ea74c29e1a8d648d0e354fb4405267630c5
TrixiePicana <152823734+TrixiePicana@users.noreply.github.com>
2025-05-04 20:29:40 +0800

    Update SuspendedState.java

--- SuspendedState.java
+++ SuspendedState.java
@@ -1,11 +1,34 @@
-public abstract class SuspendedState implements AccountState
+public class SuspendedState implements AccountState
 {
     @Override
-    public void activate()
+    public void deposit(Account account, double amount)
     {
-
+        System.out.println("You cannot deposit on a suspended account.");
     }
+    
     @Override
-    public void close() {
+    public void withdraw(Account account, double amount) 
+    {
+        System.out.println("You cannot withdraw on a suspended account.");
+    }
+
+    @Override
+    public void activate(Account account)
+    {
+        account.setAccountState(new ActiveState() );
+        System.out.println("Account is activated!");
+    }
+
+    @Override
+    public void suspend(Account account) 
+    {
+        System.out.println("Account is already suspended!");
+    }
+
+    @Override 
+    public void close(Account account)
+    {
+        account.setAccountState(new ClosedState() );
+        System.out.println("Account is closed!");
     }
 }

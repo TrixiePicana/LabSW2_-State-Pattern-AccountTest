6eb22ea62ee884c7e704da87e06a4b2babbe3fc6
TrixiePicana <152823734+TrixiePicana@users.noreply.github.com>
2025-05-04 20:19:57 +0800

    Update ActiveState.java

--- ActiveState.java
+++ ActiveState.java
@@ -1,14 +1,46 @@
-public abstract class ActiveState implements AccountState
+public class ActiveState implements AccountState
 {
     @Override
-    public void suspend()
+    public void deposit(Account account, double amount)
     {
-
+        account.setBalance(account.getBalance() + amount);
+        System.out.println("Deposit successful");
+        System.out.println(account);
     }
 
     @Override
-    public void close()
+    public void withdraw(Account account, double amount)
     {
+        if(amount > account.getBalance() )
+        {
+            System.out.println("Insufficient balance");
+        }
 
+        else
+        {
+            account.setBalance(account.getBalance() - amount);
+            System.out.println("Withdrawal successful");
+        }
+        System.out.println(account);
+    }
+
+    @Override
+    public void activate(Account account)
+    {
+        System.out.println("Account is already activated!");
+    }
+
+    @Override
+    public void suspend(Account account)
+    {
+        account.setAccountState(new SuspendedState() );
+        System.out.println("Account is suspended!");
+    }
+
+    @Override
+    public void close(Account account)
+    {
+        account.setAccountState(new ClosedState() );
+        System.out.println("Account is closed!");
     }
 }

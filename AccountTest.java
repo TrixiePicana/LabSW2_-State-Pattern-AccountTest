439f6f5723d566dd8cfafb377c3bf3bc4cd66f77
TrixiePicana <152823734+TrixiePicana@users.noreply.github.com>
2025-05-04 20:54:31 +0800

    Update AccountTest.java

--- AccountTest.java
+++ AccountTest.java
@@ -1,25 +1,21 @@
-public class AccountTest    //Client Code
+public class AccountTest //Client Code
 {
     public static void main (String[] args)
     {
-        Account myAccount = new Account("1234", 10000.0);   //Create account
-        myAccount.activate();
+        Account myAccount = new Account("1234", 10000.0);  //Create account
+        myAccount.activate(); 
 
         myAccount.suspend();
 
-        myAccount.deposit(1000.0);
+        myAccount.deposit(1000.0); //Updates balance after deposit
+        myAccount.withdraw(100.0); //Updated balance after withraw
 
-        myAccount.withdraw(100.0);
+        myAccount.close(); //A message: "Account is already closed" pops up.
+        myAccount.activate(); //A message: "You cannot activate a closed account" pops up.
+        myAccount.suspend(); //A message: "You cannot activate a closed account" pops up.
 
-        myAccount.close();
-
-        myAccount.activate();
-
-        myAccount.activate();
-
-        myAccount.withdraw(500.0);
-
-        myAccount.deposit(1000.0);
+        myAccount.withdraw(); //Disallowed, display current balance
+        myAccount.deposit(1000.0); //Disallowed, display current balance
 
     }
 }
